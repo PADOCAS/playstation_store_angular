410c7917ffbe378e341ee0246912f7a8415b4e23
Luciano Decio Neto <lucianodeciopacheco@gmail.com>
2023-09-25 13:38:40 -0300

    Implementações Playstation Store

--- src/app/components/menu-bar/menu-bar-sony/menu-bar-sony.component.ts
+++ src/app/components/menu-bar/menu-bar-sony/menu-bar-sony.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-menu-bar-sony',
   templateUrl: './menu-bar-sony.component.html',
-  styleUrls: ['./menu-bar-sony.component.css']
+  styleUrls: ['./menu-bar-sony.component.css'],
 })
 export class MenuBarSonyComponent {
+  isScrolled: boolean = false;
 
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    // Verifica a posição de rolagem e atualiza o estado do menu
+    this.isScrolled = window.scrollY > 0;
+  }
 }
